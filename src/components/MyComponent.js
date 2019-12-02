05a9acc96120f57208babceefe3c6a9dc40b6b38
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 11:46:54 +0000

    myComponent changes

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,40 +1,34 @@
-/*
- * src/components/MyComponent.js
- */
-
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { setActionTemplate } from "../actions";
-//component
-class MyComponent extends Component {
-  render() {
-    const handleOnClick = () => {
-      this.props.handleOnClick("your-item-id");
-    };
 
-    return (
-      <div>
-        <h1>My Component</h1>
-        <p>
-          <a onClick={this.handleOnClick}>Dispatch Action</a>
-        </p>
-        <p>Current item ID: {this.props.itemId}</p>
-      </div>
-    );
-  }
+//component
+export function MyComponent({ itemId, updateStateHandleOnClick }) {
+  // handleOnClick takes the id, because updateStateHandleOnClick takes an argument
+  const handleOnClick = id => {
+    updateStateHandleOnClick(id);
+  };
+  return (
+    <div>
+      <h1>My Component</h1>
+      <p>
+        <a onClick={() => handleOnClick(1)}>add1</a>
+      </p>
+      <p>
+        <a onClick={() => handleOnClick(5)}>add5</a>
+      </p>
+      <p>Current item ID: {itemId}</p>
+    </div>
+  );
 }
 
 //redux configuration:
-const mapStateToProps = (state, ownProps) => {
-  return {
-    itemId: state.itemId
-  };
-};
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    handleOnClick: itemState => dispatch(setActionTemplate(ownProps.prop))
-  };
-};
+const mapStateToProps = state => ({
+  itemId: state.child.itemId // child comes from combineReducers, itemId comes from childReducer
+});
+const mapDispatchToProps = dispatch => ({
+  updateStateHandleOnClick: itemId => dispatch(setActionTemplate(itemId)) // name it so it makes sense.
+});
 
 export const MyComponentContainer = connect(
   mapStateToProps,
