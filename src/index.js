fcfd58157b027669675c1001b372dabf2760d957
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 11:41:58 +0000

    created initialStore with child and itemId

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import combineReducers from "./reducers";
 import "./index.css";
 import App from "./App";
 
+// initialStore  neccessary for the first time page loads
+// the keys (child) should be the same as in combineReducers
 let initialStore = {
-  itemId: 1
+  child: { itemId: 9999 }
 };
 
 const store = createStore(combineReducers, initialStore);
