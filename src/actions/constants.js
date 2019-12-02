e378e2048a4ab2c4026f1f5998d720f79d0e31b1
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 14:53:58 +0000

    constant SET_USERNAME

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,4 +1,5 @@
 // Redux also suggest defining constants for your action types.
 export const setActionTypes = {
-  SET_ACTION_TYPE: "SET_ACTION_TYPE"
+  SET_ACTION_TYPE: "SET_ACTION_TYPE",
+  SET_USERNAME: "SET_USERNAME"
 };
