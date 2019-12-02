456144d6cc5ea3bba6530bc29d53fdbc7faa0fdc
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 14:55:35 +0000

    UserContainer imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MyComponentContainer from "./components/MyComponent";
+import UserContainer from "./components/User";
 
 function App() {
   return (
     <div className="App">
       <MyComponentContainer />
+      <UserContainer />
     </div>
   );
 }
