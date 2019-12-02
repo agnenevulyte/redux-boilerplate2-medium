77fbf3f2f7fd2e3ef4d630523c77eca46b1ae97c
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 11:37:42 +0000

    to access itemId in mapStateToProps

--- src/reducers/childReducer.js
+++ src/reducers/childReducer.js
@@ -1,13 +1,15 @@
 /*
  * src/reducers/childReducer.js
  */
-
+// const initialState = {
+//   itemId: 1
+// };
 export const childReducer = (state = {}, action) => {
   let newState;
 
   switch (action.type) {
     case "SET_ACTION_TYPE":
-      newState = Object.assign({}, { someValue: action.itemId });
+      newState = Object.assign({}, { itemId: action.itemId }); // to access itemId in mapStateToProps
       return newState;
 
     //define more cases as your project builds.
